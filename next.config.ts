614d5548e7547d2cc99ff720d0673ca5684f1023
Jenil Patel <patel@jenilpatel>
2026-06-23 11:12:20 +0530

    chore: move outputFileTracingRoot to config root

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingRoot: import.meta.dirname,
-  },
+  outputFileTracingRoot: import.meta.dirname,
 };
 
 export default nextConfig;
